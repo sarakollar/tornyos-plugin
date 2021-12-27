a219863af0f1dc9bfa6a0ef1042ccbab7181bae9
Sara Kollar <sara.kollar@emarsys.com>
2021-12-27 19:42:58 +0100

    Fix small numbers bug

--- brainbashers.com.js
+++ brainbashers.com.js
@@ -2,25 +2,27 @@ const fields = document.querySelectorAll('.unequaltd input');
 const dimension = Math.sqrt(fields.length);
 fields.forEach((field) => {
     field.addEventListener('keyup', (event) => {
+        console.log(field.selectionStart);
         const id = field.parentElement.id
         const position = parseInt(id.substring(2, 4));
-        changeFocus(getNewPosition(event.keyCode, position));
+        changeFocus(getNewPosition(event.keyCode, position, field.selectionStart, field.value.length));
     });
 });
 
-const getNewPosition = (keyCode, position) => {
-    if(keyCode === 37) {
+const getNewPosition = (keyCode, position, caretPosition, inputLength) => {
+    if(keyCode === 37 && caretPosition === 0 ) {
         return position - 1;
     }
     if(keyCode === 38) {
         return position - 10;
     }
-    if(keyCode === 39) {
+    if(keyCode === 39 && caretPosition === inputLength) {
         return position + 1;
     }
     if(keyCode === 40) {
         return position + 10;
     }
+    return position;
 };
 
 const changeFocus = (newPosition) => {
